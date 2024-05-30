9c45bebc961191eea99f3da98e999cc1e43778b8
Zx Loh <59832510+Zx-Loh@users.noreply.github.com>
2024-05-30 22:16:33 +0800

    Add function to display winner
    Will now display winner of the match after either player or computer hits 5 points. Reformatted code to make it cleaner. displayScore now runs only at the end of one gameplay loop.

--- script.js
+++ script.js
@@ -6,34 +6,10 @@ const scoreboard = document.querySelector("#results")
 
 const humanChoiceDisp = document.createElement("div")
 const computerChoiceDisp = document.createElement("div")
+const matchResults = document.createElement("div")
 const humanScoreDisp = document.createElement("div")
 const computerScoreDisp = document.createElement("div")
-
-
-
-options.addEventListener("click", (event) => {
-    let target = event.target
-
-    switch(target.id) {
-        case "rock":
-            playRound("rock", getComputerChoice())
-            humanChoiceDisp.textContent = `You picked ${target.id}!`
-            displayScores()
-            break;
-
-        case "paper":
-            playRound("paper", getComputerChoice())
-            humanChoiceDisp.textContent = `You picked ${target.id}!`
-            displayScores()
-            break;
-
-        case "scissors":
-            playRound("scissors", getComputerChoice())
-            humanChoiceDisp.textContent = `You picked ${target.id}!`
-            displayScores()
-            break;
-    }
-})
+const winnerDisp = document.createElement("div")
 
 
 
@@ -42,8 +18,10 @@ function displayScores () {
     humanScoreDisp.textContent = `Current Human Score: ${humanScore}`
     scoreboard.appendChild(humanChoiceDisp)
     scoreboard.appendChild(computerChoiceDisp)
+    scoreboard.appendChild(matchResults)
     scoreboard.appendChild(computerScoreDisp)
     scoreboard.appendChild(humanScoreDisp)
+    scoreboard.appendChild(winnerDisp)
 }
 
 
@@ -70,19 +48,50 @@ function getHumanChoice () {
 
 function playRound (humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
-        console.log("It's a tie!")
+        matchResults.textContent = "It's a tie!"
     
     } else if ((humanChoice == "paper" && computerChoice == "scissors") 
             ||  (humanChoice == "scissors" && computerChoice == "rock")
             ||  (humanChoice == "rock" && computerChoice == "paper")) {
-        console.log("You lose!")
+        matchResults.textContent = "You lose!"
         computerScore++;
 
     } else if ((humanChoice == "paper" && computerChoice == "rock")
             ||  (humanChoice == "scissors" && computerChoice == "paper")
             ||  (humanChoice == "rock" && computerChoice == "scissors")) {
-        console.log("You win!")
+        matchResults.textContent = "You win!"
         humanScore++;
             }
     return humanScore, computerScore
 }
+
+
+
+options.addEventListener("click", (event) => {
+    let target = event.target
+
+    switch(target.id) {
+        case "rock":
+            playRound("rock", getComputerChoice())
+            humanChoiceDisp.textContent = `You picked ${target.id}`
+            break;
+
+        case "paper":
+            playRound("paper", getComputerChoice())
+            humanChoiceDisp.textContent = `You picked ${target.id}`
+            break;
+
+        case "scissors":
+            playRound("scissors", getComputerChoice())
+            humanChoiceDisp.textContent = `You picked ${target.id}`
+            break;
+    }
+
+    if (humanScore == 5) {
+        winnerDisp.textContent = "Player won"
+    } else if (computerScore == 5) {
+        winnerDisp.textContent = "Computer won"
+    }
+
+    displayScores()
+}) 
